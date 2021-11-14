0a520d477ce6a34c7e6d44d44d7b0e5e12919e26
Daniel Sabino <40477679+D-Sabino@users.noreply.github.com>
2021-11-14 17:15:46 -0300

    Primeira aplicação Flutter
    Primeira aplicação Flutter, desenvolvimento de um contador

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -2,19 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        width: 500,
-        height: 500,
-        color: Colors.purple,
-        child: Center(
-          child: Container(
-            height: 200,
-            width: 200,
-            color: Colors.green,
-          ),
-        ),
+      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meu primeiro App"),
       ),
-    ),
-  ));
+      body: Center(
+          child: Text(
+        "CONTADOR\n$count",
+        textAlign: TextAlign.center,
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
 }
